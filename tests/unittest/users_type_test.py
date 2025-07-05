c4069bbf5800982db7dd4eca43693e2e25dcedd1
= <ggrerqq2346@rambler.ru>
2025-07-05 22:16:48 +0300

    add: Add new tests

--- tests/unittest/users_type_test.py
+++ tests/unittest/users_type_test.py
@@ -10,6 +10,26 @@ class A:
     ...
 
 
+@overload()
+def test_eq_type_foo(a: A):
+    return type(a).__name__
+
+
+@overload()
+def test_eq_type_foo(a: B):
+    return type(a).__name__
+
+
+@overload()
+def test_nq_type_foo(a: A, b: B):
+    return (type(a).__name__, type(b).__name__)
+
+
+@overload()
+def test_nq_type_foo(a: A, b: A):
+    return (type(a).__name__, type(b).__name__)
+
+
 class UsersTypeTest(TestCase):
     """
     Тесты проверяющие пользовательские типы данных
@@ -21,25 +41,64 @@ class UsersTypeTest(TestCase):
         cls.A_class = A()
 
     def test_eq_type(self):
-        @overload()
-        def foo(a: A):
-            return A.__name__
+        result_1 = test_eq_type_foo(self.A_class)
+        self.assertEqual(result_1, "A")
 
-        @overload()
-        def foo(a: B):
-            return B.__name__
-
-        self.assertEqual(foo(self.A_class), "A")
-        self.assertEqual(foo(self.B_class), "B")
+        result_2 = test_eq_type_foo(self.B_class)
+        self.assertEqual(result_2, "B")
 
     def test_nq_type(self):
-        @overload()
-        def foo(a: A, b: B):
-            return (A.__name__, B.__name__)
+        result_1 = test_nq_type_foo(self.A_class, self.B_class)
+        self.assertEqual(result_1, ("A", "B"))
 
-        @overload()
-        def foo(a: A, b: A):
-            return (A.__name__, A.__name__)
+        result_2 = test_nq_type_foo(self.A_class, self.A_class)
+        self.assertEqual(result_2, ("A", "A"))
 
-        self.assertEqual(foo(self.A_class, self.B_class), ("A", "B"))
-        self.assertEqual(foo(self.A_class, self.A_class), ("A", "A"))
+
+class Parent:
+    ...
+
+
+class Children(Parent):
+    ...
+
+
+@overload()
+def test_inheritance_foo(a: Parent):
+    return type(a).__name__
+
+
+class InheritanceTypeTest(TestCase):
+    def test_inheritance(self):
+        result_1 = test_inheritance_foo(Parent())
+        self.assertEqual(result_1, "Parent")
+
+        result_2 = test_inheritance_foo(Children())
+        self.assertEqual(result_2, "Children")
+
+
+class Parent:
+    ...
+
+
+class Children(Parent):
+    ...
+
+
+@overload()
+def test_analyz_foo(a: Parent):
+    return type(a).__name__
+
+
+@overload()
+def test_analyz_foo(a: Children):
+    return type(a).__name__
+
+
+class CurrectAnalyzTypeTest(TestCase):
+    def test_analyz(self):
+        result_1 = test_analyz_foo(Parent())
+        self.assertEqual(result_1, "Parent")
+
+        result_1 = test_analyz_foo(Children())
+        self.assertEqual(result_1, "Children")
